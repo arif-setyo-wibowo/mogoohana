c5ad30a6810cf7d35608bcbaeb8f8ebd1c4a00e4
arif-setyo-wibowo <21081010057@student.upnjatim.ac.id>
2025-02-22 17:03:28 +0700

    update seo

--- resources/views/template/home_layout.blade.php
+++ resources/views/template/home_layout.blade.php
@@ -4,14 +4,17 @@
 <!-- Meta -->
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta content="Anil z" name="author">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<meta name="description" content="Shopwise is Powerful features and You Can Use The Perfect Build this Template For Any eCommerce Website. The template is built for sell Fashion Products, Shoes, Bags, Cosmetics, Clothes, Sunglasses, Furniture, Kids Products, Electronics, Stationery Products and Sporting Goods.">
-<meta name="keywords" content="ecommerce, electronics store, Fashion store, furniture store,  bootstrap 4, clean, minimal, modern, online store, responsive, retail, shopping, ecommerce store">
+<meta property="og:title" content="Mogo Ohana Store - Buy Monopoly GO Items at the Best Prices!">
+<meta property="og:description" content="Mogo Ohana Store - Get the best deals on Monopoly GO Stickers, Packs, and Rare Cards. Fast delivery & secure transactions!">
+<meta property="og:image" content="https://https://mogoohana.store">
+<meta property="og:url" content="https://mogoohana.store">
+
 
 <!-- SITE TITLE -->
 <title>Mogo Ohana Store - Buy Monopoly GO Items at the Best Prices! 🎲</title>
+
 <!-- Favicon Icon -->
 <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/')}}/images/logo.png">
 <!-- Animation CSS -->
